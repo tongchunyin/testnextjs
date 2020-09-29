2e3ad7eaad1b7db547e5a3623936b64b58b0c645
Chun Yin Tong <>
2020-09-29 17:24:16 +0800

    remove content

--- pages/index.js
+++ pages/index.js
@@ -7,29 +7,30 @@ import Date from '../components/date'
 
 export default function Home({ allPostsData }) {
   return (
-    <Layout home>
-      {/* <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section> */}
-    </Layout>
+    <Head/>
+    // <Layout home>
+    //   <Head>
+    //     <title>{siteTitle}</title>
+    //   </Head>
+    //   <section className={utilStyles.headingMd}>
+    //   </section>
+    //   <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+    //     <h2 className={utilStyles.headingLg}>Blog</h2>
+    //     <ul className={utilStyles.list}>
+    //       {allPostsData.map(({ id, date, title }) => (
+    //         <li className={utilStyles.listItem} key={id}>
+    //           <Link href={`/posts/${id}`}>
+    //             <a>{title}</a>
+    //           </Link>
+    //           <br />
+    //           <small className={utilStyles.lightText}>
+    //             <Date dateString={date} />
+    //           </small>
+    //         </li>
+    //       ))}
+    //     </ul>
+    //   </section>
+    // </Layout>
   )
 }
 
